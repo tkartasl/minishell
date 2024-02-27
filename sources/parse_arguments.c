08537a7f6cfe4e2f7d2f08e20595e75f096a86cb
Tomi Kartaslammi <tkartasl@c1r5p11.hive.fi>
2024-02-27 14:15:04 +0200

    fixed parse_arguments

--- sources/parse_arguments.c
+++ sources/parse_arguments.c
@@ -6,37 +6,50 @@
 /*   By: tkartasl <tkartasl@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 14:15:41 by tkartasl          #+#    #+#             */
-/*   Updated: 2024/02/26 16:47:28 by tkartasl         ###   ########.fr       */
+/*   Updated: 2024/02/27 14:13:52 by tkartasl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.c"
 
-int	get_arg_count(char *cmd_line)
+void	get_arg_count(char *cmd_line, int *count)
 {
-	int		count;
-
-	count = 0;
 	while (*cmd_line != 0)
 	{
-		while(*cmd_line != ' ' && *cmd_line != 0)
-			cmd_line++;
-		cmd_line = ft_skip_whitespace(cmd_line);
-		if (*cmd_line != '<' && *cmd_line != '>')
-			count++;
-		else
+		if (*cmd_line != '<' && *cmd_line != '>' && *cmd_line != 0)
 		{
-			if ((*cmd_line == '<' || *cmd_line == '>') && (*(cmd_line + 1) == '<' || *(cmd_line + 1) == '>'))
-				cmd_line = cmd_line + 2;
-			if (*cmd_line == '<' || *cmd_line == '>')
-				cmd_line++;
-			cmd_line = ft_skip_whitespace(cmd_line);
-			while (*cmd_line != 0 && *cmd_line != ' ')
+			*count += 1;
+			while (*cmd_line != ' ' && *cmd_line != 0)
 				cmd_line++;
 			cmd_line = ft_skip_whitespace(cmd_line);
 		}
+		else
+		{
+			while (*cmd_line != 0 && (*cmd_line == '>' || *cmd_line == '<'))
+			{
+				//printf("%c\n", *cmd_line);
+				if ((*cmd_line == '<' || *cmd_line == '>') && (*(cmd_line + 1) == '<' || *(cmd_line + 1) == '>'))
+					cmd_line = cmd_line + 2;
+				if (*cmd_line == '<' || *cmd_line == '>')
+					cmd_line++;
+				cmd_line = ft_skip_whitespace(cmd_line);
+				while (*cmd_line != 0 && *cmd_line != ' ')
+					cmd_line++;
+				cmd_line = ft_skip_whitespace(cmd_line);
+			}
+		}
 	}
-	return	(count);	
+}
+
+char	*skip_redirs(char *cmd_line)
+{
+	if ((*cmd_line == '<' || *cmd_line == '>') && (*(cmd_line + 1) == '<'
+		|| *(cmd_line + 1) == '>'))
+		cmd_line = cmd_line + 2;
+	if (*cmd_line == '<' || *cmd_line == '>')
+		cmd_line++;
+	cmd_line = ft_skip_whitespace(cmd_line);
+	return (cmd_line);
 }
 
 char	*get_args(char *cmd_line)
@@ -50,48 +63,37 @@ char	*get_args(char *cmd_line)
 			return (cmd_line);
 		else
 		{
-			if ((*cmd_line == '<' || *cmd_line == '>') && (*(cmd_line + 1) == '<' || *(cmd_line + 1) == '>'))
-				cmd_line = cmd_line + 2;
-			if (*cmd_line == '<' || *cmd_line == '>')
-				cmd_line++;
-			cmd_line = ft_skip_whitespace(cmd_line);
+			cmd_line = skip_redirs(cmd_line);
 			while (*cmd_line != 0 && *cmd_line != ' ')
 				cmd_line++;
 			cmd_line = ft_skip_whitespace(cmd_line);
 			if (*cmd_line != '<' && *cmd_line != '>')
 				return (cmd_line);
-			if ((*cmd_line == '<' || *cmd_line == '>') && (*(cmd_line + 1) == '<' || *(cmd_line + 1) == '>'))
-				cmd_line = cmd_line + 2;
-			if (*cmd_line == '<' || *cmd_line == '>')
-			{	
-				cmd_line++;
-				cmd_line = ft_skip_whitespace(cmd_line);
-			}
+			cmd_line = skip_redirs(cmd_line);
 		}
 	}
 	return (0);
 }
 
-char	**parse_arguments(char *line)
+char	**parse_arguments(char *line, char *line2)
 {
 	char	**args;
-	char	*temp;
 	int		i;
 	int		count;
 	int		len;
 
+	count = 0;
 	len = 0;
-	temp = line;
 	i = 0;
-	count = get_arg_count(temp);
+	get_arg_count(line, &count);
 	args = malloc((count + 1) * sizeof(char *));
 	if (args == 0)
 		printf("Error");
 	while (i < count)
 	{
-		line = get_args(line);
-		len = get_len(line);
-		args[i] = ft_strndup(line, len);
+		line2 = get_args(line2);
+		len = get_len(line2);
+		args[i] = ft_strndup(line2, len);
 		if (args[i] == 0)
 			printf("Error");
 		i++;
