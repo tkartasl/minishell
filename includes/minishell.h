c3941570a213ddec98aa9b1239b9a99156c0518b
Ville Savolainen <vsavolai@c1r1p9.hive.fi>
2024-02-29 14:31:33 +0200

    syntax functions added to header

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: vsavolai <vsavolai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 10:38:23 by tkartasl          #+#    #+#             */
-/*   Updated: 2024/02/28 16:21:03 by vsavolai         ###   ########.fr       */
+/*   Updated: 2024/02/29 14:30:39 by vsavolai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,15 +36,16 @@ typedef struct s_cmd_args
 	int			pipe_count;
 }				t_cmd_args;
 
-void		parse_line(char	*line);
-char    	*find_redir(char *str);
-int			get_len_check_error(char *str);
+void        parse_line(char	*line);
+char        *find_redir(char *str);
+int         get_len_check_error(char *str);
 int			get_len(char *str);
 char		*find_limiter(char *str);
 void		build_list(t_redir **heredocs, char *lim, int index);
 t_cmd_args	**get_array(t_redir **redir, t_redir **hdoc, char **line, int pipe);
 char		**parse_arguments(char *line, char *line2);
 char		*skip_redirs(char *cmd_line);
-void		check_syntax_error(char *str);
-int			check_empty_line(char **lines);
+void        syntax_error(char **cmd_line);
+int         check_syntax(char **cmd_lines);
+
 #endif
