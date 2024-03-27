3443c3cfcd59e53033540d7051a3d3bc546d1aea
Tomi Kartaslammi <tkartasl@c1r1p11.hive.fi>
2024-03-27 09:44:09 +0200

    progress on exit & echo functions

--- sources/builtins.c
+++ sources/builtins.c
@@ -6,17 +6,24 @@
 /*   By: tkartasl <tkartasl@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 12:53:05 by tkartasl          #+#    #+#             */
-/*   Updated: 2024/03/25 15:02:41 by tkartasl         ###   ########.fr       */
+/*   Updated: 2024/03/26 09:10:28 by tkartasl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	echo()
+void	echo(char **args, int fd)
 {
-
-
+	int	i;	
+	int	j;
 	
+	i = 0;
+	j = 0;
+	while (args[i] != 0)
+	{
+		ft_putstr_fd(args[i], fd);
+		i++;
+	}
 }
 
 void	cd(char	*path)
--- sources/builtins2.c
+++ sources/builtins2.c
@@ -6,7 +6,7 @@
 /*   By: tkartasl <tkartasl@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 12:53:36 by tkartasl          #+#    #+#             */
-/*   Updated: 2024/02/16 12:20:27 by tkartasl         ###   ########.fr       */
+/*   Updated: 2024/03/26 12:11:02 by tkartasl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,18 @@ void	env()
 	
 }
 
-void	exit()
+void	exit(char *status, t_env **env_table, t_cmd_args **cmd_args);
 {
+	char	*temp;
+	int		i;
+
+	i = 0;
+	temp = status;
+	while (*temp != 0 && ft_isdigit(*temp) == 1)
+		temp++;
+	if (*temp == 0)
+		i = ft_atoi(status);
 	
-	
+
+	exit(i);
 }
