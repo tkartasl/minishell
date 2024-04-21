2855390ceebe1392c67f9f64c73a0b339940bf25
Ville Savolainen <vsavolai@c1r1p9.hive.fi>
2024-04-21 17:19:36 +0300

    adde pipe_utils.c

---
+++ sources/pipe_utils.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipe_utils.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vsavolai <vsavolai@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/21 12:32:19 by vsavolai          #+#    #+#             */
+/*   Updated: 2024/04/21 16:00:00 by vsavolai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+char	**get_cmd(t_cmd_args *cmd_args)
+{
+	int		i;
+	int		j;
+	char	**cmd;
+
+	i = 1;
+	j = 0;
+	while (cmd_args->args[j] != 0)
+	{
+		i++;
+		j++;
+	}
+	cmd = malloc(sizeof(char *) * (i + 1));
+	if (cmd == NULL)
+	{
+		ft_putstr_fd("minishell: error allocating memory\n", 2);
+		return (NULL);
+	}
+	cmd[0] = cmd_args->cmd;
+	i = 1;
+	j = 0;
+	while (cmd_args->args[j] != 0)
+		cmd[i++] = cmd_args->args[j++];
+	cmd[i] = 0;
+	return (cmd);
+}
+
+int	init_pipe_vars(int *i, int *fd1, int **process_ids, int pc)
+{
+	*process_ids = ft_calloc(pc + 1, sizeof(int));
+	if (process_ids == NULL)
+		return (-1);
+	*i = -1;
+	*fd1 = 0;
+	return (0);
+}
+
+void	wait_children(int *process_ids, int fd2, t_env **et)
+{
+	int	i;
+	int	status;
+
+	i = 0;
+	while (process_ids[i])
+	{
+		waitpid(process_ids[i], &status, 0);
+		i++;
+	}
+	free(process_ids);
+	change_cmd_status(et, status);
+	close(fd2);
+}
+
+void	null_cmd_hdoc(t_cmd_args *ca, int *flag)
+{
+	char	*filename;
+	t_redir	*temp;
+
+	temp = *ca->head_hdocs;
+	while (temp)
+	{
+		*flag = check_h_docs(temp, 0, &filename);
+		if (flag > 0)
+			close(*flag);
+		temp = temp->next;
+	}
+}
